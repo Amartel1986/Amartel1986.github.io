a4bfbf62a1ccf110fe30d3a0bd7a49efa57a1192
Aleksey Popovich <Amartel@tut.by>
2019-11-14 14:29:35 +0300

    Adds C# dump script.

---
+++ csharp-dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DOTNET_SDK_VERSION=3.0.100
+wget -O dotnet.tar.gz https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VERSION/dotnet-sdk-$DOTNET_SDK_VERSION-linux-musl-x64.tar.gz
+
+mkdir -p /usr/share/dotnet 
+tar -C /usr/share/dotnet -xzf dotnet.tar.gz 
+rm -f /usr/bin/dotnet 
+ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet 
+rm dotnet.tar.gz
+
+dotnet tool install --global dotnet-dump
+export PATH=$PATH:$HOME/.dotnet/tools
+
+dotnet-dump collect --process-id 1
